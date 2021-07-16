7a50ce99c514003740c59e946c75b04ba25afc47
Denis Mazur <denismazur8@gmail.com>
2021-07-17 02:14:46 +0300

    fix: remove bad test

--- test/unary_handler_test.go
+++ test/unary_handler_test.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"math"
 	"testing"
-	"time"
 
 	"github.com/libp2p/go-libp2p-core/protocol"
 )
@@ -74,71 +73,6 @@ func TestUnaryCalls(t *testing.T) {
 			t.Logf("remote correctly returned error: '%v'\n", err)
 		},
 	)
-}
-
-// TODO: create benchmark
-func TestConcurrentUnaryCalls(t *testing.T) {
-	t.Skip("skipping slow test")
-	_, p1, cancel1 := createDaemonClientPair(t)
-	_, p2, cancel2 := createDaemonClientPair(t)
-
-	t.Cleanup(func() {
-		cancel1()
-		cancel2()
-	})
-
-	peer1ID, peer1Addrs, err := p1.Identify()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if err := p2.Connect(peer1ID, peer1Addrs); err != nil {
-		t.Fatal(err)
-	}
-
-	var proto protocol.ID = "sqrt"
-	p1.NewUnaryHandler(
-		proto,
-		func(data []byte) ([]byte, error) {
-			f := float64FromBytes(data)
-			if f < 0 {
-				return nil, fmt.Errorf("can't extract square root from negative")
-			}
-
-			result := math.Sqrt(f)
-			return float64Bytes(result), nil
-		},
-	)
-
-	// TODO: add flags
-	numJobs := 1000
-	cnt := make(chan struct{})
-
-	for i := 0; i < numJobs; i++ {
-		go func(i int) {
-			f := float64(i)
-			_, err := p2.UnaryCall(peer1ID, proto, float64Bytes(f))
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			cnt <- struct{}{}
-		}(i)
-	}
-
-	counter := 0
-	before := time.Now()
-loop:
-	for {
-		select {
-		case <-cnt:
-			counter++
-			if counter == numJobs {
-				break loop
-			}
-		}
-	}
-
-	t.Logf("success: %d jobs competed in %v\n", numJobs, time.Since(before))
 }
 
 func float64FromBytes(bytes []byte) float64 {
