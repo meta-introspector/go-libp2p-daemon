93a332ed6b88c52e2c75e5d5af5c368daef6f0aa
justheuristic <justheuristic@gmail.com>
2022-12-08 10:58:16 +0300

    un-verbose

--- test/unary_handler_test.go
+++ test/unary_handler_test.go
@@ -10,7 +10,6 @@ import (
 	"testing"
 	"time"
 
-	logging "github.com/ipfs/go-log"
 	"github.com/libp2p/go-libp2p-daemon/p2pclient"
 	"github.com/libp2p/go-libp2p/core/protocol"
 	ma "github.com/multiformats/go-multiaddr"
@@ -18,11 +17,6 @@ import (
 )
 
 func TestConcurrentCalls(t *testing.T) {
-	lvl, err := logging.LevelFromString("debug")
-	if err != nil {
-		panic(err)
-	}
-	logging.SetAllLoggers(lvl)
 	_, p1, cancel1 := createDaemonClientPair(t)
 	_, p2, cancel2 := createDaemonClientPair(t)
 	time.Sleep(100 * time.Millisecond) // not sure this is useful
