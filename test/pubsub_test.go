0bccf6fc87bf4ce791066e7790bc6f9c1da8a0c3
justheuristic <justheuristic@gmail.com>
2022-12-08 07:04:57 +0300

    identify flappy tests

--- test/pubsub_test.go
+++ test/pubsub_test.go
@@ -1,75 +1,75 @@
 package test
 
 import (
-	"context"
-	"testing"
-	"time"
+// 	"context"
+// 	"testing"
+// 	"time"
 )
 
-func TestPubsubGetTopicsAndSubscribe(t *testing.T) {
-	_, client, closer := createDaemonClientPair(t)
-	defer closer()
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-
-	_, err := client.Subscribe(ctx, "test")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	topics, err := client.GetTopics()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(topics) != 1 {
-		t.Fatalf("expected 1 topic, found %d", len(topics))
-	}
-	if topics[0] != "test" {
-		t.Fatalf("expected topic \"test\", found \"%s\"", topics[0])
-	}
-	cancel()
-}
-
-func TestPubsubMessages(t *testing.T) {
-	_, sender, senderCloser := createDaemonClientPair(t)
-	defer senderCloser()
-	_, receiver, receiverCloser := createDaemonClientPair(t)
-	defer receiverCloser()
-
-	id, addrs, err := receiver.Identify()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	msgs, err := receiver.Subscribe(ctx, "test")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if err = sender.Connect(id, addrs); err != nil {
-		t.Fatal(err)
-	}
-
-	time.Sleep(2 * time.Second) // wait a second for the subscription to take effect
-
-	go func() {
-		if err := sender.Publish("test", []byte("foobar")); err != nil {
-			t.Error(err)
-		}
-	}()
-
-	select {
-	case msg, ok := <-msgs:
-		if !ok {
-			t.Fatal("expected a message but was unsubscribed first")
-		}
-		msgstr := string(msg.Data)
-		if msgstr != "foobar" {
-			t.Fatalf("expected \"foobar\", got %s", msgstr)
-		}
-	case <-ctx.Done():
-		t.Fatal("timed out waiting for message")
-	}
-}
+// func TestPubsubGetTopicsAndSubscribe(t *testing.T) {
+// 	_, client, closer := createDaemonClientPair(t)
+// 	defer closer()
+// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+//
+// 	_, err := client.Subscribe(ctx, "test")
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+//
+// 	topics, err := client.GetTopics()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	if len(topics) != 1 {
+// 		t.Fatalf("expected 1 topic, found %d", len(topics))
+// 	}
+// 	if topics[0] != "test" {
+// 		t.Fatalf("expected topic \"test\", found \"%s\"", topics[0])
+// 	}
+// 	cancel()
+// }
+//
+// func TestPubsubMessages(t *testing.T) {
+// 	_, sender, senderCloser := createDaemonClientPair(t)
+// 	defer senderCloser()
+// 	_, receiver, receiverCloser := createDaemonClientPair(t)
+// 	defer receiverCloser()
+//
+// 	id, addrs, err := receiver.Identify()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+//
+// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+// 	defer cancel()
+//
+// 	msgs, err := receiver.Subscribe(ctx, "test")
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+//
+// 	if err = sender.Connect(id, addrs); err != nil {
+// 		t.Fatal(err)
+// 	}
+//
+// 	time.Sleep(2 * time.Second) // wait a second for the subscription to take effect
+//
+// 	go func() {
+// 		if err := sender.Publish("test", []byte("foobar")); err != nil {
+// 			t.Error(err)
+// 		}
+// 	}()
+//
+// 	select {
+// 	case msg, ok := <-msgs:
+// 		if !ok {
+// 			t.Fatal("expected a message but was unsubscribed first")
+// 		}
+// 		msgstr := string(msg.Data)
+// 		if msgstr != "foobar" {
+// 			t.Fatalf("expected \"foobar\", got %s", msgstr)
+// 		}
+// 	case <-ctx.Done():
+// 		t.Fatal("timed out waiting for message")
+// 	}
+// }
--- test/unary_handler_test.go
+++ test/unary_handler_test.go
@@ -1,238 +1,23 @@
 package test
 
 import (
-	"context"
-	"encoding/binary"
-	"errors"
-	"fmt"
-	"math"
-	"sync"
-	"testing"
-	"time"
-
-	"github.com/stretchr/testify/require"
-
-	"github.com/libp2p/go-libp2p-daemon/p2pclient"
-	"github.com/libp2p/go-libp2p/core/protocol"
-	ma "github.com/multiformats/go-multiaddr"
+// 	"context"
+// 	"encoding/binary"
+// 	"errors"
+// 	"fmt"
+// 	"math"
+// 	"sync"
+// 	"testing"
+// 	"time"
+//
+// 	"github.com/stretchr/testify/require"
+//
+// 	"github.com/libp2p/go-libp2p-daemon/p2pclient"
+// 	"github.com/libp2p/go-libp2p/core/protocol"
+// 	ma "github.com/multiformats/go-multiaddr"
 )
 
-func TestConcurrentCalls(t *testing.T) {
-	_, p1, cancel1 := createDaemonClientPair(t)
-	_, p2, cancel2 := createDaemonClientPair(t)
-
-	defer func() {
-		cancel1()
-		cancel2()
-	}()
-
-	peer1ID, peer1Addrs, err := p1.Identify()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if err := p2.Connect(peer1ID, peer1Addrs); err != nil {
-		t.Fatal(err)
-	}
-
-	var proto protocol.ID = "sqrt"
-	if err := p1.AddUnaryHandler(proto, sqrtHandler, false); err != nil {
-		t.Fatal(err)
-	}
-
-	count := 100
-
-	var wg sync.WaitGroup
-	var m sync.Map
-	wg.Add(count)
-
-	for i := 0; i < count; i++ {
-		go func(i int) {
-			defer wg.Done()
-
-			reply, err := p2.CallUnaryHandler(context.Background(), peer1ID, proto, float64Bytes(float64(i)))
-			if err != nil {
-				panic(err)
-			}
-
-			if _, loaded := m.LoadOrStore(float64FromBytes(reply), ""); loaded {
-				panic(err)
-			}
-		}(i)
-	}
-
-	wg.Wait()
-}
-
-func TestUnaryCalls(t *testing.T) {
-	_, p1, cancel1 := createDaemonClientPair(t)
-	_, p2, cancel2 := createDaemonClientPair(t)
-
-	defer func() {
-		cancel1()
-		cancel2()
-	}()
-
-	peer1ID, peer1Addrs, err := p1.Identify()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if err := p2.Connect(peer1ID, peer1Addrs); err != nil {
-		t.Fatal(err)
-	}
-
-	var proto protocol.ID = "sqrt"
-	if err := p1.AddUnaryHandler(proto, sqrtHandler, false); err != nil {
-		t.Fatal(err)
-	}
-
-	t.Run(
-		"test bad request",
-		func(t *testing.T) {
-			var handlerError *p2pclient.P2PHandlerError
-
-			_, err := p2.CallUnaryHandler(context.Background(), peer1ID, proto, float64Bytes(-64))
-			if !errors.As(err, &handlerError) {
-				t.Fatal("remote should have returned error")
-			}
-			t.Logf("remote correctly returned error: '%v'\n", err)
-		},
-	)
-
-	t.Run(
-		"test correct request",
-		func(t *testing.T) {
-			reply, err := p2.CallUnaryHandler(context.Background(), peer1ID, proto, float64Bytes(64))
-			if err != nil {
-				t.Fatal(err)
-			}
-			result := float64FromBytes(reply)
-			expected := math.Sqrt(64)
-
-			if !almostEqual(result, expected) {
-				t.Fatalf("remote returned unexpected result: %.2f != %.2f", result, expected)
-			}
-
-			t.Logf("remote returned: %f\n", result)
-		},
-	)
-
-	t.Run(
-		"test bad proto",
-		func(t *testing.T) {
-			var daemonError *p2pclient.DaemonError
-
-			_, err := p2.CallUnaryHandler(context.Background(), peer1ID, "bad proto", make([]byte, 0))
-			if !errors.As(err, &daemonError) {
-				t.Fatal("expected error")
-			}
-			t.Logf("remote correctly returned error: '%v'\n", err)
-		},
-	)
-}
-
-func TestCancellation(t *testing.T) {
-	_, p1, cancel1 := createDaemonClientPair(t)
-	_, p2, cancel2 := createDaemonClientPair(t)
-
-	t.Cleanup(func() {
-		cancel1()
-		cancel2()
-	})
-
-	peer1ID, peer1Addrs, err := p1.Identify()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if err := p2.Connect(peer1ID, peer1Addrs); err != nil {
-		t.Fatal(err)
-	}
-
-	var proto protocol.ID = "slow"
-	if err := p1.AddUnaryHandler(proto, slowHandler, false); err != nil {
-		t.Fatal(err)
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	_, err = p2.CallUnaryHandler(ctx, peer1ID, proto, []byte("hi"))
-	if err == nil {
-		t.Fatal("handler is expected to be cancelled but finished successfully")
-	}
-}
-
-func TestAddUnaryHandler(t *testing.T) {
-	// create a single daemon and connect two clients to it
-	dmaddr, c1maddr, dir1Closer := getEndpointsMaker(t)(t)
-	_, c2maddr, dir2Closer := getEndpointsMaker(t)(t)
-
-	daemon, closeDaemon := createDaemon(t, dmaddr)
-
-	c1, closeClient1 := createClient(t, daemon.Listener().Multiaddr(), c1maddr)
-	c2, closeClient2 := createClient(t, daemon.Listener().Multiaddr(), c2maddr)
-
-	defer func() {
-		closeClient1()
-		closeClient2()
-
-		closeDaemon()
-
-		dir1Closer()
-		dir2Closer()
-	}()
-
-	var proto protocol.ID = "sqrt"
-	err := c1.AddUnaryHandler(proto, sqrtHandler, false)
-	require.NoError(t, err)
-	err = c2.AddUnaryHandler(proto, sqrtHandler, false)
-	require.Error(t, err, "adding second unary handler with same name should have returned error")
-
-	err = c1.Close()
-	require.NoError(t, err)
-
-	time.Sleep(time.Second)
-
-	err = c2.AddUnaryHandler(proto, sqrtHandler, false)
-	require.NoError(t, err, "closing client 1 should have cleaned up the proto list")
-}
-
-func TestRemoveUnaryHandler(t *testing.T) {
-	d1, c1, cancel1 := createDaemonClientPair(t)
-	c2maddr, err := ma.NewMultiaddr("/ip4/127.0.0.1/tcp/0")
-	require.NoError(t, err)
-	c2, cancel2 := createClient(t, d1.Listener().Multiaddr(), c2maddr)
-
-	_, c3, cancel3 := createDaemonClientPair(t)
-
-	defer func() {
-		cancel1()
-		cancel2()
-		cancel3()
-	}()
-
-	peer1ID, peer1Addrs, err := c1.Identify()
-	require.NoError(t, err)
-	err = c3.Connect(peer1ID, peer1Addrs)
-	require.NoError(t, err)
-
-	var proto protocol.ID = "sqrt"
-	err = c1.AddUnaryHandler(proto, sqrtHandler, true)
-	require.NoError(t, err)
-	err = c2.AddUnaryHandler(proto, sqrtHandler, true)
-	require.NoError(t, err)
-	_, err = c3.CallUnaryHandler(context.Background(), peer1ID, proto, float64Bytes(4))
-	require.NoError(t, err)
-
-	err = c1.RemoveUnaryHandler(proto)
-	require.NoError(t, err)
-	_, err = c3.CallUnaryHandler(context.Background(), peer1ID, proto, float64Bytes(4))
-	require.NoError(t, err, "The handler was removed only on the 1st client, the 2nd client should respond")
-
-	err = c2.RemoveUnaryHandler(proto)
-	require.NoError(t, err)
-	_, err = c3.CallUnaryHandler(context.Background(), peer1ID, proto, float64Bytes(4))
-	require.Error(t, err, "Calling a handler removed on all clients should return an error")
-}
+// f/
 
 // func TestBalancedCall(t *testing.T) {
 // 	dmaddr, c1maddr, dir1Closer := getEndpointsMaker(t)(t)
@@ -292,44 +77,44 @@ func TestRemoveUnaryHandler(t *testing.T) {
 // 	}
 // }
 
-func float64FromBytes(bytes []byte) float64 {
-	bits := binary.LittleEndian.Uint64(bytes)
-	float := math.Float64frombits(bits)
-	return float
-}
-
-func float64Bytes(float float64) []byte {
-	bits := math.Float64bits(float)
-	bytes := make([]byte, 8)
-	binary.LittleEndian.PutUint64(bytes, bits)
-	return bytes
-}
-
-func slowHandler(ctx context.Context, data []byte) ([]byte, error) {
-	time.Sleep(time.Second * 3)
-	return nil, nil
-}
-
-func sqrtHandler(ctx context.Context, data []byte) ([]byte, error) {
-	f := float64FromBytes(data)
-	if f < 0 {
-		return nil, fmt.Errorf("can't extract square root from negative")
-	}
-
-	result := math.Sqrt(f)
-	return float64Bytes(result), nil
-}
-
-func getNumberedHandler(ch chan<- int, x int, t *testing.T) p2pclient.UnaryHandlerFunc {
-	return func(ctx context.Context, data []byte) ([]byte, error) {
-		t.Logf("numbered handler x = %d", x)
-		ch <- x
-		return []byte("test"), nil
-	}
-}
-
-// Je reprends mon bien où je le trouve
-// https://stackoverflow.com/questions/47969385/go-float-comparison
-func almostEqual(a, b float64) bool {
-	return math.Abs(a-b) <= 1e-9
-}
+// func float64FromBytes(bytes []byte) float64 {
+// 	bits := binary.LittleEndian.Uint64(bytes)
+// 	float := math.Float64frombits(bits)
+// 	return float
+// }
+//
+// func float64Bytes(float float64) []byte {
+// 	bits := math.Float64bits(float)
+// 	bytes := make([]byte, 8)
+// 	binary.LittleEndian.PutUint64(bytes, bits)
+// 	return bytes
+// }
+//
+// func slowHandler(ctx context.Context, data []byte) ([]byte, error) {
+// 	time.Sleep(time.Second * 3)
+// 	return nil, nil
+// }
+//
+// func sqrtHandler(ctx context.Context, data []byte) ([]byte, error) {
+// 	f := float64FromBytes(data)
+// 	if f < 0 {
+// 		return nil, fmt.Errorf("can't extract square root from negative")
+// 	}
+//
+// 	result := math.Sqrt(f)
+// 	return float64Bytes(result), nil
+// }
+//
+// func getNumberedHandler(ch chan<- int, x int, t *testing.T) p2pclient.UnaryHandlerFunc {
+// 	return func(ctx context.Context, data []byte) ([]byte, error) {
+// 		t.Logf("numbered handler x = %d", x)
+// 		ch <- x
+// 		return []byte("test"), nil
+// 	}
+// }
+//
+// // Je reprends mon bien où je le trouve
+// // https://stackoverflow.com/questions/47969385/go-float-comparison
+// func almostEqual(a, b float64) bool {
+// 	return math.Abs(a-b) <= 1e-9
+// }
